19a00e9af1c71b565411cd27007de721108b2e40
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2026-03-16 16:31:07 +0100

    2 Remove aggregation logic

--- libs/angular-components/table/src/lib/table.component.ts
+++ libs/angular-components/table/src/lib/table.component.ts
@@ -35,12 +35,10 @@ import { TableData } from './entities/table-data';
 import { TableRow } from './entities/table-row';
 import { Toolbar } from './entities/toolbar';
 import { ToolbarAction } from './entities/toolbar-action';
-import { Aggregation } from './entities/aggregations';
 
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { PaginatorComponent } from './paginator/paginator.component';
 import { PageSelectorComponent } from './page-selector/page-selector.component';
-import { AggregationRowComponent } from './aggregation-row/aggregation-row.component';
 import { CellData } from './entities/cell-data-type';
 import {
   renderBooleanTemplate,
@@ -51,7 +49,7 @@ import {
   renderProgressTemplate,
   renderStatusTemplate,
 } from './cell-renderer/renderer-templates';
-import { BitMask, customCellRendererMask, numbersOnlyMask, Types } from './entities/types-bitmask';
+import { customCellRendererMask, Types } from './entities/types-bitmask';
 import { left } from '@popperjs/core';
 
 @Component({
@@ -75,7 +73,6 @@ export class TableComponent implements AfterViewInit, OnChanges, OnInit {
   @Input() isSelectionEnabled = true;
   @Input() columnsSizeStrategy: 'fitGridWidth' | 'fitCellContents' | 'fitProvidedWidth' = 'fitProvidedWidth';
   @Input() rowGroupPanelShow: 'always' | 'onlyWhenGrouping' | 'never' | undefined = 'onlyWhenGrouping';
-  @Input() showAggRow = false;
   @Input() dateFormat = 'yyyy-MM-dd';
   @Input() allowRowGrouping = true;
   @Input() pagination = true;
@@ -87,8 +84,6 @@ export class TableComponent implements AfterViewInit, OnChanges, OnInit {
   @ViewChild('myGrid') grid!: AgGridAngular;
   @ViewChild('myGrid', { read: ElementRef }) gridRef!: ElementRef;
 
-  bottomRowData: any[] = [{}];
-  appliedAggFuncs: { [key in Aggregation]?: boolean } = {};
   columns: TableColumn[] = [];
   rows: TableRow[] = [];
   columnTypes!: { [key: string]: ColDef };
@@ -129,7 +124,6 @@ export class TableComponent implements AfterViewInit, OnChanges, OnInit {
     this.setUpRowHeight();
     this.setUpSideBar();
     this.setUpAutoSizing();
-    this.setUpAggRow();
   }
 
   ngAfterViewInit(): void {
@@ -138,7 +132,7 @@ export class TableComponent implements AfterViewInit, OnChanges, OnInit {
 
   @HostListener('keydown', ['$event'])
   keydown(event: KeyboardEvent) {
-    const activeElement = (<any>event).explicitOriginalTarget;
+    const activeElement = (event as any).explicitOriginalTarget;
     if (activeElement?.tagName === 'INPUT' || !event.shiftKey) {
       return;
     }
@@ -245,137 +239,17 @@ export class TableComponent implements AfterViewInit, OnChanges, OnInit {
     ];
   }
 
-  private showIcon = (aggFunc: Aggregation) => {
-    if (this.appliedAggFuncs[aggFunc]) {
-      return '<i class="icon i-lxt-check"></i>';
-    }
-    return '';
-  };
-
   private getMainMenuItems = (params: any) => {
     let items = params.defaultItems;
     if (!this.allowRowGrouping) {
       items = items.filter((item: any) => item !== 'rowGroup');
     }
-    items.push({
-      name: 'Aggregation func.',
-      subMenu: this.getAggregationContextMenu(params),
-      icon: '<i class="fa fa-star"></i>',
-      disabled: false,
-    });
     return items;
   };
 
   private getContextMenuItems = (params: any) => {
-    // Context menu for aggregation row
-    if (params.node.id.match(/b-[0-9]{1,2}/g)) {
-      return [];
-    }
-
     return this.getRowContextMenu();
   };
-
-  private getAggregationContextMenu(params: any) {
-    if (!this.grid.columnDefs) {
-      return [];
-    }
-    const types = new BitMask();
-    for (const columnDef of this.grid.columnDefs) {
-      const column = (<ColDef>columnDef).field;
-      if (!column) {
-        continue;
-      }
-
-      const type = this.grid.api.getColumn(column)?.getUserProvidedColDef()?.cellDataType ?? 'text';
-      switch (type) {
-        case 'text':
-        case 'boolean':
-        case 'checkbox':
-        case 'ordinalNumber':
-          types.setFlag(Types.text);
-          break;
-        case 'number':
-        case 'status':
-        case 'progress':
-          types.setFlag(Types.number);
-          break;
-        case 'date':
-          types.setFlag(Types.date);
-          break;
-      }
-    }
-    const defalutActions = [
-      {
-        name: 'count',
-        icon: this.showIcon(Aggregation.COUNT),
-        action: () => {
-          this.addAggregation(Aggregation.COUNT);
-        },
-      },
-      {
-        name: 'count_d',
-        icon: this.showIcon(Aggregation.COUNT_DISTINCT),
-        action: () => {
-          this.addAggregation(Aggregation.COUNT_DISTINCT);
-        },
-      },
-      {
-        name: 'first',
-        icon: this.showIcon(Aggregation.FIRST),
-        action: () => {
-          this.addAggregation(Aggregation.FIRST);
-        },
-      },
-      {
-        name: 'last',
-        icon: this.showIcon(Aggregation.LAST),
-        action: () => {
-          this.addAggregation(Aggregation.LAST);
-        },
-      },
-      {
-        name: 'avg',
-        icon: this.showIcon(Aggregation.AVERAGE),
-        action: () => {
-          this.addAggregation(Aggregation.AVERAGE);
-        },
-        disabled: !types.checkFlag(Types.number),
-      },
-      {
-        name: 'median',
-        icon: this.showIcon(Aggregation.MEDIAN),
-        action: () => {
-          this.addAggregation(Aggregation.MEDIAN);
-        },
-        disabled: !types.checkFlag(Types.number),
-      },
-      {
-        name: 'sum',
-        icon: this.showIcon(Aggregation.SUM),
-        action: () => {
-          this.addAggregation(Aggregation.SUM);
-        },
-        disabled: !types.checkFlag(Types.number),
-      },
-      {
-        name: 'max',
-        icon: this.showIcon(Aggregation.MAX),
-        action: () => {
-          this.addAggregation(Aggregation.MAX);
-        },
-        disabled: !types.checkFlag(Types.number | Types.date),
-      },
-      {
-        name: 'min',
-        icon: this.showIcon(Aggregation.MIN),
-        action: () => {
-          this.addAggregation(Aggregation.MIN);
-        },
-        disabled: !types.checkFlag(Types.number | Types.date),
-      },
-    ];
-    return defalutActions;
-  }
 
   private getRowContextMenu(): (string | MenuItemDef)[] {
     const result: (string | MenuItemDef)[] = [];
@@ -424,17 +298,6 @@ export class TableComponent implements AfterViewInit, OnChanges, OnInit {
       localeText: {
         values: 'Row groups values',
       },
-      aggFuncs: {
-        min: this.minAggregation.bind(this),
-        max: this.maxAggregation.bind(this),
-        sum: this.sumAggregation.bind(this),
-        avg: this.averageAggregation.bind(this),
-        first: this.firstAggregation.bind(this),
-        last: this.lastAggregation.bind(this),
-        count: this.countAggregation.bind(this),
-        count_d: this.countDistinctAggregation.bind(this),
-        median: this.medianAggregation.bind(this),
-      },
       suppressRowClickSelection: !this.isSelectionEnabled,
       statusBar: this.pagination
         ? {
@@ -479,215 +342,11 @@ export class TableComponent implements AfterViewInit, OnChanges, OnInit {
   }
 
   protected reload(): void {
-    const aggFuncs = this.bottomRowData.map((aggFunc) =>
-      Object.keys(aggFunc).map((key) => ({ col: key, aggType: aggFunc[key].aggFunction })),
-    );
-    this.bottomRowData = [{}];
-    for (const aggRow of aggFuncs) {
-      for (const foo of aggRow) {
-        this.addAggregation(foo.aggType);
-      }
-    }
     if (this.grid.api.getDisplayedRowCount() === 0) {
       this.grid.api.showNoRowsOverlay();
     } else {
       this.grid.api.hideOverlay();
     }
-  }
-
-  private setUpAggRow(): void {
-    if (!this.showAggRow) {
-      return;
-    }
-    this.bottomRowData = [{}];
-  }
-
-  private addAggregation(aggType: Aggregation): void {
-    this.appliedAggFuncs[aggType] = !this.appliedAggFuncs[aggType];
-    if (!this.grid.columnDefs) {
-      return;
-    }
-    for (const columnDef of this.grid.columnDefs) {
-      const allRows: { colDefs: ColDef; values: any[] } = { colDefs: {}, values: [] };
-      const column = (<ColDef>columnDef).field;
-      const type = (<ColDef>columnDef).type as keyof typeof Types;
-
-      if (!column) {
-        continue;
-      }
-      allRows.colDefs = columnDef;
-      let index = this.bottomRowData.findIndex((row) => row[column]?.aggFunction === aggType);
-      if (index >= 0) {
-        delete this.bottomRowData[index][column];
-        for (let i = index + 1; i < this.bottomRowData.length; i++) {
-          if (this.bottomRowData[i][column]) {
-            this.bottomRowData[index++][column] = this.bottomRowData[i][column];
-          }
-          delete this.bottomRowData[i][column];
-        }
-        this.bottomRowData = this.bottomRowData.filter((row) => Object.keys(row).length > 0);
-        if (this.bottomRowData.length === 0) {
-          this.bottomRowData = [{}];
-        }
-        this.bottomRowData = [...this.bottomRowData];
-        continue;
-      }
-
-      this.grid.api.forEachNodeAfterFilterAndSort((node) => {
-        if (!node.group) {
-          allRows.values.push(node.data[column]);
-        }
-      });
-      let value: number | string = 0;
-      switch (aggType) {
-        case Aggregation.SUM:
-          if (numbersOnlyMask.checkFlag(Types[type])) {
-            value = this.sumAggregation(allRows);
-          }
-          break;
-        case Aggregation.COUNT:
-          value = this.countAggregation(allRows);
-          break;
-        case Aggregation.COUNT_DISTINCT:
-          value = this.countDistinctAggregation(allRows);
-          break;
-        case Aggregation.AVERAGE:
-          value = this.averageAggregation(allRows);
-          break;
-        case Aggregation.FIRST:
-          value = this.firstAggregation(allRows);
-          break;
-        case Aggregation.LAST:
-          value = this.lastAggregation(allRows);
-          break;
-        case Aggregation.MAX:
-          value = this.maxAggregation(allRows);
-          break;
-        case Aggregation.MIN:
-          value = this.minAggregation(allRows);
-          break;
-        case Aggregation.MEDIAN:
-          value = this.medianAggregation(allRows);
-          break;
-        default:
-          break;
-      }
-      const object = { aggFunction: aggType, value: value };
-      //const emptyIndex = this.bottomRowData.findIndex(
-      //  (row) =>
-      //    !row[column] && (Object.entries(row)?.findIndex((entry: any) => entry['aggFunction'] !== aggType) ?? -1) >= 0,
-      //);
-      //console.log(emptyIndex);
-      const lastIndex = this.bottomRowData.length - 1;
-      const entries = Object.entries(this.bottomRowData[lastIndex]);
-
-      if (value === '') {
-        continue;
-      }
-      if (entries.length === 0 || entries.find((entry: any) => entry[1]?.aggFunction === aggType)) {
-        this.bottomRowData[lastIndex][column] = object;
-      } else {
-        this.bottomRowData.push({ [column]: object });
-      }
-      this.bottomRowData = [...this.bottomRowData];
-    }
-    if (Object.keys(this.bottomRowData[0]).length === 0) {
-      this.showAggRow = false;
-    } else {
-      this.showAggRow = true;
-    }
-  }
-
-  firstAggregation(rows: any): string {
-    rows = rows.values;
-    return rows[0] ?? '';
-  }
-
-  lastAggregation(rows: any) {
-    rows = rows.values;
-    return rows.at(-1) ?? '';
-  }
-
-  countAggregation(rows: any) {
-    rows = rows.values;
-    return rows.length;
-  }
-
-  countDistinctAggregation(rows: any) {
-    rows = rows.values;
-    return new Set(rows).size;
-  }
-
-  medianAggregation(rows: any) {
-    if (!this.isColTypeNumber(rows.colDef)) {
-      return '';
-    }
-    rows = rows.values;
-    if (!rows || !rows.every) {
-      return '';
-    }
-    if (!rows.every((item: any) => typeof item === 'number' && !isNaN(item))) {
-      return '';
-    }
-    const sortedRows = rows.sort((a: any, b: any) => a - b);
-    const condition = !!(rows.length % 2) || isNaN(rows[0]);
-    const index = Math.floor(rows.length / 2);
-    const value = condition ? sortedRows[index] : (sortedRows[index] + sortedRows[index + 1]) / 2;
-    return value;
-  }
-
-  minAggregation(rows: any) {
-    rows = rows.values;
-    if (!rows.every || !rows.every((item: any) => (typeof item === 'number' && !isNaN(item)) || item.getTime)) {
-      return '';
-    }
-    return rows.sort((a: any, b: any) => a - b)[0] ?? '';
-  }
-
-  maxAggregation(rows: any) {
-    rows = rows.values;
-    if (!rows.every || !rows.every((item: any) => (typeof item === 'number' && !isNaN(item)) || item.getTime)) {
-      return '';
-    }
-    return rows.sort((a: any, b: any) => b - a)[0] ?? '';
-  }
-
-  sumAggregation(rows: any) {
-    if (!this.isColTypeNumber(rows.colDef)) {
-      return '';
-    }
-    rows = rows.values;
-    if (!rows) {
-      return '';
-    }
-    let sum = 0;
-    for (const row of rows) {
-      if (isNaN(row) || row?.getTime) {
-        return '';
-      }
-      sum += row;
-    }
-    return sum;
-  }
-
-  averageAggregation(rows: any) {
-    if (!this.isColTypeNumber(rows.colDef)) {
-      return '';
-    }
-    rows = rows.values;
-    if (!rows) {
-      return '';
-    }
-    const sum = this.sumAggregation(rows);
-    if (isNaN(<number>sum)) {
-      return '';
-    }
-    return <number>sum / rows.length;
-  }
-
-  isColTypeNumber(colDef: ColDef) {
-    const type = colDef.cellDataType as keyof typeof Types;
-    return numbersOnlyMask.checkFlag(Types[type]);
   }
 
   private setUpColumnTypes(): void {
@@ -726,66 +385,53 @@ export class TableComponent implements AfterViewInit, OnChanges, OnInit {
             component: 'agGroupCellRenderer',
           };
         }
-        if (params.node.rowPinned) {
+        const type = params.colDef?.cellDataType as keyof typeof Types;
+        if (!customCellRendererMask.checkFlag(Types[type])) {
           return {
-            component: AggregationRowComponent,
-            params: {
-              ...params,
-              autoHeight: true,
-              columns: this.tableData.columns,
-              bottomRowData: this.bottomRowData,
-              dateFormat: this.dateFormat,
-            },
-          };
-        } else {
-          const type = params.colDef?.cellDataType as keyof typeof Types;
-          if (!customCellRendererMask.checkFlag(Types[type])) {
-            return {
-              component: undefined,
-            };
-          }
-          return {
-            component: (params: ICellRendererParams & TableColumn) => {
-              let value = '';
-              let aggFunc;
-              const group = params.node.group ?? false;
-              value = params?.value ?? '';
-              if (group) {
-                aggFunc = params.column?.getAggFunc() as string;
-              }
-              const dateFormat = params['dateFormat'];
-              const columnType =
-                params['columns'].find((column: TableColumn) => column.field === params.column?.getColId())
-                  ?.cellDataType ?? CellData.TEXT;
-
-              if (value || !group) {
-                switch (columnType) {
-                  case 'boolean':
-                    return renderBooleanTemplate(value, aggFunc ?? '');
-                  case 'checkbox':
-                    return renderCheckboxTemplate(value, aggFunc ?? '');
-                  case 'date':
-                    return renderDateTemplate(value, aggFunc ?? '', dateFormat);
-                  case 'ordinalNumber':
-                    return renderOrdinalNumberTemplate(value, aggFunc ?? '');
-                  case 'status':
-                    return renderStatusTemplate(value, aggFunc ?? '');
-                  case 'progress':
-                    return renderProgressTemplate(value, aggFunc ?? '');
-                  default:
-                    return renderDefaultTemplate(value);
-                }
-              } else {
-                return '';
-              }
-            },
-            params: {
-              ...params,
-              columns: this.tableData.columns,
-              dateFormat: this.dateFormat,
-            },
+            component: undefined,
           };
         }
+        return {
+          component: (params: ICellRendererParams & TableColumn) => {
+            let value = '';
+            let aggFunc;
+            const group = params.node.group ?? false;
+            value = params?.value ?? '';
+            if (group) {
+              aggFunc = params.column?.getAggFunc() as string;
+            }
+            const dateFormat = params['dateFormat'];
+            const columnType =
+              params['columns'].find((column: TableColumn) => column.field === params.column?.getColId())
+                ?.cellDataType ?? CellData.TEXT;
+
+            if (value || !group) {
+              switch (columnType) {
+                case 'boolean':
+                  return renderBooleanTemplate(value, aggFunc ?? '');
+                case 'checkbox':
+                  return renderCheckboxTemplate(value, aggFunc ?? '');
+                case 'date':
+                  return renderDateTemplate(value, aggFunc ?? '', dateFormat);
+                case 'ordinalNumber':
+                  return renderOrdinalNumberTemplate(value, aggFunc ?? '');
+                case 'status':
+                  return renderStatusTemplate(value, aggFunc ?? '');
+                case 'progress':
+                  return renderProgressTemplate(value, aggFunc ?? '');
+                default:
+                  return renderDefaultTemplate(value);
+              }
+            } else {
+              return '';
+            }
+          },
+          params: {
+            ...params,
+            columns: this.tableData.columns,
+            dateFormat: this.dateFormat,
+          },
+        };
       },
     };
 
